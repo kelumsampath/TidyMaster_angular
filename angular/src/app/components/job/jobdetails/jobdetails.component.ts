09f8482b2213762913f699775c18a03d3aee3ac1
kelum <edirisingheemks@gmail.com>
2018-09-11 18:52:49 +0530

    final interim

--- angular/src/app/components/job/jobdetails/jobdetails.component.ts
+++ angular/src/app/components/job/jobdetails/jobdetails.component.ts
@@ -12,7 +12,16 @@ export class JobdetailsComponent implements OnInit {
   postid:any;
   title:String;
   status:String;
-  jobdate:Date;
+  jobdate:String;
+  levelofjob:String;
+  employeegender:String;
+  priceperhour:String;
+  estimatedtime:String;
+  numberofcleaners:Number;
+  joblocation:String;
+  splitted:any;
+  date:String;
+  
   constructor(
               private activatedRoute: ActivatedRoute,
               private authservice:AuthService,
@@ -26,10 +35,15 @@ export class JobdetailsComponent implements OnInit {
       if(res.state){
         
           this.title=res.job.title;
-          this.status=res.job.status;
           this.jobdate=res.job.jobdate;
-          
-
+          this.levelofjob=res.job.levelofjob;
+          this.employeegender=res.job.employeegender;
+          this.priceperhour=res.job.priceperhour;
+          this.estimatedtime=res.job.estimatedtime;
+          this.numberofcleaners=res.job.numberofcleaners;
+          this.joblocation=res.job.joblocation;
+          this.splitted = this.jobdate.split("T",2);
+          this.date=this.splitted[0];
       }
         else{
           this.ngFlashMessageService.showFlashMessage({messages: ["SERVER ERROR OCCUERED!"],dismissible: true,timeout: 4000,type: 'danger'});
