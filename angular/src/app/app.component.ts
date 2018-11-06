04f60a54ec21541d2fe731ab654d20c84212b255
kelum <edirisingheemks@gmail.com>
2018-11-06 15:08:14 +0530

    admin identification after refresh

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from "./service/auth.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(
+    private authservice:AuthService
+  ){
+    this.authservice.isadmin().subscribe(res=>{
+      if(res.state){
+        this.authservice.adminorsuperadmin=true;
+        }
+      })
+  }
+
 }
